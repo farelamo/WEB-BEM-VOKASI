71e4080f76cd254f0315464ccafd87e8f7bf1f93
farelamo <farizfahmi674@gmail.com>
2021-09-07 23:44:05 +0700

    Add Login and Register

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
