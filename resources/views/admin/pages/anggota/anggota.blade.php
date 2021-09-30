d11b7fafff68a4044d509e6bad5d5609206bfc5f
Naufal Ulinnuha <naufalulinnuha40@gmail.com>
2021-09-30 20:54:38 +0700

    Fix SPA Anggota

--- resources/views/admin/pages/anggota/anggota.blade.php
+++ resources/views/admin/pages/anggota/anggota.blade.php
@@ -14,7 +14,7 @@
 
         {{-- BUTTON TEST SPA --}}
 
-        <button type="button" class="btn btn-primary" style="margin-right: 10px" 
+        <!-- <button type="button" class="btn btn-primary" style="margin-right: 10px" 
         data-toggle="collapse" data-target="#buat" aria-expanded="false" aria-controls="collapseExample" id="btntampil" onclick="tampil()">
             Tambah SPA
         </button>
@@ -22,6 +22,10 @@
         <button type="button" class="btn btn-primary" style="margin-right: 10px; display: none" 
         data-toggle="collapse" data-target="#buat" aria-expanded="false" aria-controls="collapseExample" onclick="sembunyi()" id="btnsembunyi">
             Tambah SPA
+        </button> -->
+
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#buat" aria-expanded="true" aria-controls="collapseExample">
+            Colapse
         </button>
 
         {{-- END TEST SPA --}}
@@ -32,74 +36,74 @@
     </div>
 
     {{-- TESTING SPA --}}
-    
-    <div class="section-body" style="display: none;" id="buat" data-aos="fade-up">
+
+    <div class="section-body collapse" id="buat">
         <div class="card">
             <div class="card-body">
-                    <form action="/anggota" method="post" enctype="multipart/form-data" class="needs-validation" novalidate="">
-                        @csrf
+            <form action="/anggota" method="post" enctype="multipart/form-data" class="needs-validation" novalidate="">
+                    @csrf
+
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="nama" placeholder="Masukkan Nama" required>
+                        <div class="invalid-feedback"> <h6>Nama Harus diisi</h6> </div>
+                        <div class="text-danger"> @error('nama') {{ $message }} @enderror </div>
+                    </div>
 
                         <div class="form-group">
-                            <input type="text" class="form-control" name="nama" placeholder="Masukkan Nama" required>
-                            <div class="invalid-feedback"> <h6>Nama Harus diisi</h6> </div>
-                            <div class="text-danger"> @error('nama') {{ $message }} @enderror </div>
-                        </div>
+                        <select class="form-control" name="gender" id="gender" required>
+                            <option value="">-- Pilih Gender -- </option>
+                            <option value="L">Laki - Laki</option>
+                            <option value="P">Perempuan</option>
+                        </select>
+                        <div class="invalid-feedback"> <h6>Pilih salah satu gender !!</h6> </div>
+                        @error('gender')
+                            <div class="alert alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
 
-                          <div class="form-group">
-                            <select class="form-control" name="gender" id="gender" required>
-                                <option value="">-- Pilih Gender -- </option>
-                                <option value="L">Laki - Laki</option>
-                                <option value="P">Perempuan</option>
-                            </select>
-                            <div class="invalid-feedback"> <h6>Pilih salah satu gender !!</h6> </div>
-                            @error('gender')
-                                <div class="alert alert-danger">
-                                    {{ $message }}
-                                </div>
-                            @enderror
-                        </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="prodi" placeholder="Prodi" required>
+                        <div class="invalid-feedback"> <h6>Prodi Harus diisi</h6> </div>
+                        <div class="text-danger"> @error('prodi') {{ $message }} @enderror </div>
+                    </div>
 
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="prodi" placeholder="Prodi" required>
-                            <div class="invalid-feedback"> <h6>Prodi Harus diisi</h6> </div>
-                            <div class="text-danger"> @error('prodi') {{ $message }} @enderror </div>
-                        </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="jabatan" placeholder="Masukkan Jabatan" required>
+                        <div class="invalid-feedback"> <h6>Jabatan Harus diisi</h6> </div>
+                        <div class="text-danger"> @error('jabatan') {{ $message }} @enderror </div>
+                    </div>
 
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="jabatan" placeholder="Masukkan Jabatan" required>
-                            <div class="invalid-feedback"> <h6>Jabatan Harus diisi</h6> </div>
-                            <div class="text-danger"> @error('jabatan') {{ $message }} @enderror </div>
-                        </div>
+                    <div class="form-group">
+                        <select class="form-control" name="kepengurusan" id="kepengurusan">
+                            <option value="">-- Pilih Jenis Kepengurusan -- </option>
+                            @foreach ($urus as $item)
+                                <option value="{{ $item->id }}" >{{ $item->jenis }}</option>
+                            @endforeach
+                        </select>
+                        <div class="invalid-feedback"> <h6>Kepengurusan Harus diisi</h6> </div>
+                        @error('kepengurusan')
+                            <div class="alert alert-danger">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
 
-                        <div class="form-group">
-                            <select class="form-control" name="kepengurusan" id="kepengurusan">
-                                <option value="">-- Pilih Jenis Kepengurusan -- </option>
-                                @foreach ($urus as $item)
-                                    <option value="{{ $item->id }}" >{{ $item->jenis }}</option>
-                                @endforeach
-                            </select>
-                            <div class="invalid-feedback"> <h6>Kepengurusan Harus diisi</h6> </div>
-                            @error('kepengurusan')
-                                <div class="alert alert-danger">
-                                    {{ $message }}
-                                </div>
-                            @enderror
-                        </div>
+                    <div class="form-group">
+                        <p for="foto">Upload Foto :</p>
+                        <input type="file" class="form-control" name="foto" style="height: auto" required>
+                        <div class="invalid-feedback"><h6>Foto Harus diisi</h6> </div>
+                        <div class="text-danger"> @error('foto') {{ $message }} @enderror </div>
+                    </div>
 
-                        <div class="form-group">
-                            <p for="foto">Upload Foto :</p>
-                            <input type="file" class="form-control" name="foto" style="height: auto" required>
-                            <div class="invalid-feedback"><h6>Foto Harus diisi</h6> </div>
-                            <div class="text-danger"> @error('foto') {{ $message }} @enderror </div>
-                        </div>
-
-                        <hr>
-                        <div class="modal-footer p-0 pt-3">
-                            <button type="button" data-dismiss="modal" class="btn btn-secondary">Cancel</button>
-                            <button type="submit" class="btn btn-primary btn-shadow">
-                                <i class="fa fa-check"></i><span> Submit</span></button>
-                        </div>
-                    </form>
+                    <hr>
+                    <div class="modal-footer p-0 pt-3">
+                        <button type="button" data-dismiss="modal" class="btn btn-secondary">Cancel</button>
+                        <button type="submit" class="btn btn-primary btn-shadow">
+                            <i class="fa fa-check"></i><span> Submit</span></button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
