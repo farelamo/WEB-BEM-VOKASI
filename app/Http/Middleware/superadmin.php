cfd574e079aafff5083ae6fa1f9d50b4c5b55043
farelamo <farizfahmi674@gmail.com>
2021-10-03 02:14:29 +0700

    Add Toast Login

--- app/Http/Middleware/superadmin.php
+++ app/Http/Middleware/superadmin.php
@@ -19,7 +19,7 @@ class superadmin
         if(auth()->user()->superadmin == 1){
             return $next($request);
         } else {
-            return redirect('/dashboard')->with('message', 'Anda bukan Superadmin !!');
+            return redirect('/dashboard')->with('error', 'Anda bukan Superadmin !!');
         }
     }
 }
